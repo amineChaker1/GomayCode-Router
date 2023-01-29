c71e2e8469d1c190f7eb58a96c9870d0ed3e837f
Chaker Amine <chkramine2003@gmail.com>
2023-01-29 05:54:40 +0100

    react router documentation

---
+++ router-doc/src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Products from "./components/Products";
+import FeaturedProducts from "./components/Featured";
+import NewProducts from "./components/NewProducts";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />}>
+          <Route path="featured" element={<FeaturedProducts />} />
+          <Route path="new" element={<NewProducts />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
